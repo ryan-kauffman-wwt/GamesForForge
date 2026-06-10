f2b4cb2ed123064120cb59a713e6116ed47cdb2e
Forge <forge@example.com>
2026-06-10 15:35:41 +0000

    Add Memory Game to the application

---
+++ src/data/memoryGameData.js
@@ -0,0 +1,10 @@
+export const memoryGameCards = [
+  { id: 1, value: 'A' },
+  { id: 2, value: 'A' },
+  { id: 3, value: 'B' },
+  { id: 4, value: 'B' },
+  { id: 5, value: 'C' },
+  { id: 6, value: 'C' },
+  { id: 7, value: 'D' },
+  { id: 8, value: 'D' },
+];
